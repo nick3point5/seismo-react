8ca0174ea80e701ee7b67e771db2f6fd8c9dcbcd
kennytrinh <kenny.trinh4444@gmail.com>
2021-03-11 10:09:17 -0600

    wss

--- src/config/Api.jsx
+++ src/config/Api.jsx
@@ -1,10 +1,10 @@
 let seismoApiUrl, wsSeismoApiUrl
 if (process.env.REACT_APP_PRODUCTION) {
   seismoApiUrl = `https://${process.env.REACT_APP_SEISMO_API_URL}`
-  wsSeismoApiUrl = `ws://${process.env.REACT_APP_SEISMO_API_URL}`
+  wsSeismoApiUrl = `wss://${process.env.REACT_APP_SEISMO_API_URL}`
 } else {
   seismoApiUrl = 'http://localhost:4000'
-  wsSeismoApiUrl = 'ws://localhost:4000'
+  wsSeismoApiUrl = 'wss://localhost:4000'
 }
 
 export { wsSeismoApiUrl }
