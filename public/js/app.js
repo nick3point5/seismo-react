82d7a50c5765256a4ff1f2de6eb67533fcf3238e
kennytrinh <kenny.trinh4444@gmail.com>
2021-03-11 14:10:31 -0600

    toned done background

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,10 @@
 document.body.onload = function () {
 	function rippleEffect() {
 		const body = document.body;
-		const fillRatio = 0.5;
+		const fillRatio = 0.3;
 		const area = body.clientHeight * body.clientWidth;
-		const speed = 1;
-		const num_ripples = 40;
+		const speed = 2;
+		const num_ripples = area/40000*fillRatio;
 
 		function getRand(max, min) {
 			let num = Math.random() * (max + 1 - min) + min - 1;
@@ -20,7 +20,7 @@ document.body.onload = function () {
 			const ripple = $(
 				`<div id="${
 					"ripple" + i
-				}" class="ripple"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="margin: auto; background: none; display: block; shape-rendering: auto;" width="200px" height="200px" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid" ><circle cx="50" cy="50" r="0" fill="none" stroke="#ffff00" stroke-width="8"><animate attributeName="r" repeatCount="indefinite" dur="1s" values="0;40" keyTimes="0;1" keySplines="0 0.2 0.8 1" calcMode="spline" begin="0s"></animate><animate attributeName="opacity" repeatCount="indefinite" dur="1s" values="1;0" keyTimes="0;1" keySplines="0.2 0 0.8 1" calcMode="spline" begin="0s"></animate></circle><circle cx="50" cy="50" r="0" fill="none" stroke="#ff0000" stroke-width="8"><animate attributeName="r" repeatCount="indefinite" dur="1s" values="0;40" keyTimes="0;1" keySplines="0 0.2 0.8 1" calcMode="spline" begin="-0.5s"></animate><animate attributeName="opacity" repeatCount="indefinite" dur="1s" values="1;0" keyTimes="0;1" keySplines="0.2 0 0.8 1" calcMode="spline" begin="-0.5s"></animate></circle></svg></div>`
+				}" class="ripple"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="margin: auto; background: none; display: block; shape-rendering: auto;" width="200px" height="200px" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid" ><circle cx="50" cy="50" r="0" fill="none" stroke="#ffff00" stroke-width="8"><animate attributeName="r" repeatCount="indefinite" dur="${speed}s" values="0;40" keyTimes="0;1" keySplines="0 0.2 0.8 1" calcMode="spline" begin="0s"></animate><animate attributeName="opacity" repeatCount="indefinite" dur="${speed}s" values="1;0" keyTimes="0;1" keySplines="0.2 0 0.8 1" calcMode="spline" begin="0s"></animate></circle><circle cx="50" cy="50" r="0" fill="none" stroke="#ff0000" stroke-width="8"><animate attributeName="r" repeatCount="indefinite" dur="${speed}s" values="0;40" keyTimes="0;1" keySplines="0 0.2 0.8 1" calcMode="spline" begin="-${speed/2}s"></animate><animate attributeName="opacity" repeatCount="indefinite" dur="${speed}s" values="1;0" keyTimes="0;1" keySplines="0.2 0 0.8 1" calcMode="spline" begin="-${speed/2}s"></animate></circle></svg></div>`
 			);
 			ripple.appendTo("body");
 			$("#ripple" + i)[0].style.transform = `translate(${x}px,${-y}px)`;
@@ -37,7 +37,7 @@ document.body.onload = function () {
 			if (i === num_ripples) {
 				i = 0;
 			}
-		}, 500);
+		}, speed*1000);
 	}
 
 	function bodyShake() {
