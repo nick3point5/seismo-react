ea12ff8f4a098f391f9471a5fb0c969b37b23353
kennytrinh <kenny.trinh4444@gmail.com>
2021-03-11 09:54:42 -0600

    flip production/local

--- src/config/Api.jsx
+++ src/config/Api.jsx
@@ -1,10 +1,10 @@
 let seismoApiUrl, wsSeismoApiUrl
 if (process.env.REACT_APP_PRODUCTION) {
-  seismoApiUrl = 'http://localhost:4000'
-  wsSeismoApiUrl = 'ws://localhost:4000'
-} else {
   seismoApiUrl = `https://${process.env.REACT_APP_SEISMO_API_URL}`
   wsSeismoApiUrl = `ws://${process.env.REACT_APP_SEISMO_API_URL}`
+} else {
+  seismoApiUrl = 'http://localhost:4000'
+  wsSeismoApiUrl = 'ws://localhost:4000'
 }
 
 export { wsSeismoApiUrl }
